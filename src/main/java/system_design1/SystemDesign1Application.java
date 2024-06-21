e41c16da6a41588a3213cc7202e030aa450d3bcc
Jung-YouHwan <yhwjd99@gmail.com>
2024-06-21 17:08:00 +0900

    feat : 초기 설정

---
+++ src/main/java/system_design1/SystemDesign1Application.java
@@ -0,0 +1,13 @@
+package system_design1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemDesign1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemDesign1Application.class, args);
+    }
+
+}
---
+++ src/test/java/system_design1/SystemDesign1ApplicationTests.java
@@ -0,0 +1,13 @@
+package system_design1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemDesign1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
